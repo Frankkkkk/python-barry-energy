43ce5eaeedeba6094063a44f5e7fd138265f852e
Frank Villaro-Dixon <frank@vi-di.fr>
2021-02-17 22:09:08 +0100

    setup: change package name

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-    name = "python-barry-energy-api",
+    name = "python-barry-energy",
     version = "0.0.1",
     author = "Frank Villaro-Dixon",
     author_email = "frank@villaro-dixon.eu",
